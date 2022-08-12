d254acc064f0fd56d723448431fe6c7037e4654b
Ugur Can Ozturk <57688057+ugur99@users.noreply.github.com>
2022-08-12 23:39:00 +0300

    logging refactor for job component

--- src/job/job.py
+++ src/job/job.py
@@ -1,21 +1,47 @@
+from logger import get_logger
 from subprocess import run
 from prometheus_client import CollectorRegistry, push_to_gateway
 import prometheus_client as prom
 import os
 
 directory = '/node/'
-registry = CollectorRegistry()
+registry = CollectorRegistry(),
+logger = get_logger()
 
 def calculation(dir):
     process = run(['du', '-sb', dir], capture_output=True, text=True)
     size = process.stdout.split()[0]
     return size
 
-for key in os.environ:
-    nodeName = os.environ["NODE_NAME"]
-    pvc = os.environ["PVC_NAMES"]
-    podName = os.environ["POD_NAME"]
-    registryUrl = os.environ["PUSHGATEWAY_PROMETHEUS_PUSHGATEWAY_SERVICE_HOST"] + ":" + os.environ["PUSHGATEWAY_PROMETHEUS_PUSHGATEWAY_SERVICE_PORT"]
+
+try:
+  os.environ["NODE_NAME"]
+  nodeName = os.environ["NODE_NAME"]
+except KeyError:
+  logger.error("NODE_NAME was not set, please check the env variables.")
+  exit(1)
+
+try:
+  os.environ["PUSHGATEWAY_URL"]
+except KeyError:
+  logger.warning("PUSHGATEWAY_URL was not set, defaulting to PUSHGATEWAY_PROMETHEUS_PUSHGATEWAY_SERVICE_HOST:PUSHGATEWAY_PROMETHEUS_PUSHGATEWAY_SERVICE_PORT")
+  registryUrl = os.environ["PUSHGATEWAY_PROMETHEUS_PUSHGATEWAY_SERVICE_HOST"] + ":" + os.environ["PUSHGATEWAY_PROMETHEUS_PUSHGATEWAY_SERVICE_PORT"]
+else:
+  registryUrl = os.environ["PUSHGATEWAY_URL"]    
+
+try:
+  os.environ["PVC_NAMES"]
+  pvc = os.environ["PVC_NAMES"]
+except KeyError:
+  logger.error("PVC_NAMES was not set, please check the env variables.")
+  exit(1)
+
+try:
+  os.environ["POD_NAME"]
+  podName = os.environ["POD_NAME"]
+except KeyError:
+  logger.error("POD_NAME was not set, please check the env variables.")
+  exit(1)
 
 
 claims=pvc.split(',')
@@ -27,5 +53,7 @@ for i in os.listdir(directory):
         # TODO: FIX THIS TO HANDLES PVC NAMES WITH CONTAINING THE SAME STRING; E.G. pvc1, pvc11, pvc111
         if claims[j] in i:
             filename=(directory+"/"+i)
+            logger.debug("Found PVC: " + claims[j] + " in directory: " + filename)
             gauge.labels(claims[j],nodeName).set(calculation(filename))
             push_to_gateway(registryUrl, job="projectalpha", registry=registry)
+            logger.debug("Pushed metrics to registry: " + registryUrl)
---
+++ src/job/logger.py
@@ -0,0 +1,24 @@
+import logging, os
+
+def get_logger():
+    logger = logging.getLogger(__name__)
+
+    try:
+        os.environ["LOG_LEVEL"]
+    except KeyError:
+        logger.warning("LOG_LEVEL was not set, defaulting to INFO")
+        logLevel = "INFO"
+    else:
+        logger.warning("LOG_LEVEL was set: " + os.environ["LOG_LEVEL"].upper())
+        logLevel = os.environ["LOG_LEVEL"].upper()
+
+    logger.setLevel(logging.getLevelName(logLevel))
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    ConsoleOutputHandler = logging.StreamHandler()
+    ConsoleOutputHandler.setLevel(logging.DEBUG)
+    ConsoleOutputHandler.setFormatter(formatter)
+
+    logger.addHandler(ConsoleOutputHandler)
+
+    return logger
